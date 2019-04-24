ff47b0d9faff1fce7dbaa0ee3544df2514f035c8
kmcelwee <kevinrmcelwee@gmail.com>
2019-04-24 14:58:06 -0400

    updates

--- russia.js
+++ russia.js
@@ -1,6 +1,13 @@
 // attempted
 var y = {"2005": ["MANAFORT"], "2006": ["COHEN", "MANAFORT", "SESSIONS"], "2007": ["TRUMP", "MANAFORT"], "2008": ["TRUMP"], "2011": ["MANAFORT"], "2012": ["MANAFORT"], "2013": ["TRUMP", "COHEN", "MANAFORT", "PAGE"], "2014": ["MAJOR", "INVESTIGATIONS", "RUSSIANS", "FLYNN", "SESSIONS"], "2015": ["MAJOR", "INVESTIGATIONS", "RUSSIANS", "TRUMP", "COHEN", "MANAFORT", "FLYNN", "SESSIONS"], "2016": ["MAJOR", "INVESTIGATIONS", "RUSSIANS", "TRUMP", "COHEN", "MANAFORT", "KUSHNER", "JR", "FLYNN", "PAPADOPOULOS", "PAGE", "SESSIONS"], "2018": ["MAJOR", "INVESTIGATIONS", "RUSSIANS", "TRUMP", "COHEN", "MANAFORT", "KUSHNER", "JR", "FLYNN", "PAPADOPOULOS", "PAGE", "SESSIONS"], "2017": ["MAJOR", "INVESTIGATIONS", "RUSSIANS", "TRUMP", "COHEN", "MANAFORT", "KUSHNER", "JR", "FLYNN", "PAPADOPOULOS", "PAGE", "SESSIONS"], "2019": ["MAJOR", "RUSSIANS", "COHEN", "SESSIONS"]};
 
+var y = {'1998': [], '1999 - 2000': [], '2001': [], '2004': [], '2005': ['MANAFORT'], '2006': ['COHEN', 'MANAFORT', 'SESSIONS'], '2007': ['TRUMP', 'MANAFORT'], '2008': ['TRUMP'], '2011': ['MANAFORT'], '2012': ['MANAFORT'], '2013': ['PAGE', 'COHEN', 'TRUMP', 'MANAFORT'], '2014': ['MAJOR', 'FLYNN', 'SESSIONS', 'RUSSIANS', 'INVESTIGATIONS'], '2015': ['MANAFORT',
+  'RUSSIANS',  'SESSIONS',  'MAJOR',  'INVESTIGATIONS',  'TRUMP',  'FLYNN',  'COHEN'],
+ '2016': ['COHEN',  'RUSSIANS',  'MAJOR',  'FLYNN',  'SESSIONS',  'MANAFORT',  'PAPADOPOULOS',  'PAGE',  'TRUMP',  'INVESTIGATIONS',  'JR',  'KUSHNER'],
+ '2018': ['MAJOR',  'SESSIONS',  'INVESTIGATIONS',  'TRUMP',  'MANAFORT',  'PAGE',  'COHEN',  'JR',  'RUSSIANS',  'FLYNN',  'PAPADOPOULOS',  'KUSHNER'],
+ '2017': ['TRUMP',  'FLYNN',  'INVESTIGATIONS',  'KUSHNER',  'COHEN',  'SESSIONS',  'RUSSIANS',  'MANAFORT',  'MAJOR',  'PAPADOPOULOS',  'PAGE',  'JR'],
+ '2019': ['SESSIONS', 'COHEN', 'MAJOR', 'RUSSIANS']}
+
 var cal = `
 	<div class="cal">
 		<span style="width: 8.47%;">Jan</span>
@@ -39,6 +46,17 @@ function toggle() {
 				arr[i].style['transition'] = 'visibility 0ms 500ms, height 500ms ease-out, opacity 100ms ease-in';
 			}
 		}
+		if (arr[i].classList.contains('above_item')) {
+			if (arr[i].style['width'] == '0px') {
+				arr[i].style['width'] = '25px';
+				arr[i].style['visibility'] = 'visible';
+				arr[i].style['transition'] = 'visibility 0ms 0ms, width 500ms ease-out, opacity 100ms ease-in';
+			} else {
+				arr[i].style['width'] = '0px';
+				arr[i].style['visibility'] = 'hidden';
+				arr[i].style['transition'] = 'visibility 0ms 500ms, width 500ms ease-out, opacity 100ms ease-in';
+			}
+		}
 	}
 }
 
@@ -47,21 +65,35 @@ function createYear(j, y) {
 	for (var i = 0; i < j.length; i++) {
 		return_s += `<div id="${y}_${j[i]}" class="time_container ${j[i]}"></div>`;
 	}
-	return_s += cal + '<br>';
+	if (['2004', '2001', '1999 - 2000', '1998'].indexOf(y) < 0) {
+		return_s += cal;
+	}
+	return_s += '<br>';
 	return return_s;
 }
 
 function years() {
-	l = ['2005', '2006', '2007', '2008', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'];
+	l = ['1998', '1999 - 2000', '2001', '2004', '2005', '2006', '2007', '2008', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'];
 	output = "";
 	for (var i = l.length - 1; i >= 0; i--) {
-		output += `<div id="${l[i]}"><h2>${l[i]}</h2>${createYear(y[l[i]], l[i])}</div>`;
+		output += `<div id="${l[i]}">
+					<h2>${l[i]}</h2>
+					<div class="above_container" id="${l[i]}_sub"></div>
+					${createYear(y[l[i]], l[i])}
+				</div>`;
 	}
 	document.getElementById("all_years").innerHTML = output;
 }
 
-function createAnnual(j) {
-	return_s = "";
+function createAbove(m) {
+	if (m.time_description != null) {
+		var time = m.time_description + ' - ';
+	} else {
+		var time = ''
+	}
+	return `<a href="${m.url}" target="_blank">
+		<div class="above_item ${m.cat_id}" tooltip="${time} ${m.info}" tooltip-position="right"></div>
+	</a>`;
 }
 
 function createRange(j) {
@@ -70,13 +102,10 @@ function createRange(j) {
 		1: 34,
 		2: 28,
 	};
-	if (j.start_p < 10) {
-		var pos = "right"
-	}
-	else if (j.start_p > 90) {
-		var pos = "left"
-	}
-	else { var pos = "top"}
+	
+	if (j.start_p < 10) { var pos = "right"; }
+	else if (j.start_p > 90) { var pos = "left"; }
+	else { var pos = "top"; }
 
 	var return_s = (`<a href="${j.url}" target="_blank"><div class="date_range" tooltip="${j.time_description} - ${j.info}"
 			tooltip-position="${pos}"
@@ -115,31 +144,38 @@ function createCircle(j) {
 	return return_s;
 }
 
-function circles() {
-  output = "";
-
-  document.getElementById("circle_spot").innerHTML = output;
-}
-
 
 function all_items(all_j) {
 	for (var year in y) {
 		for (var i = 0; i < y[year].length; i++) {
 			cat = y[year][i];
-			output = "";
+			var output = "";
 			for (var key in all_j) {
 				var m = all_j[key];
-				if (m["year"] == year && m["cat_id"] == cat && m["end"] == null) {
+				if (m["start"] != null) {
+					if (m["year"] == year && m["cat_id"] == cat && m["end"] == null) {
 						output += createCircle(m);
 					}
-				if (m["year"] == year && m["cat_id"] == cat && m["end"] != null) {
+					if (m["year"] == year && m["cat_id"] == cat && m["end"] != null) {
 						output += createRange(m);
 					}
 				}
+			}
 			var id_text = `${year}_${cat}`;
 			document.getElementById(id_text).innerHTML = output;
 		}
 	}
+
+	for (var year in y) {
+		var output = "";
+		for (var key in all_j) {
+			var m = all_j[key];
+			if (m['year'] == year && m["start"] == null) { output += createAbove(m); }
+		}
+		var id_text = `${year}_sub`;
+		document.getElementById(id_text).innerHTML = output;	
+	}
+	
 }
 
 function check_mobile() {
